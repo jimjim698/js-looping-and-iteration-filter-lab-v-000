e437701596d778563961f0a1d0f80b9c3cd64e9a
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-09 00:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code your solution in this file
 
 function findMatching(drivers, string){
- return  drivers.filter(function (d) {d.toLowerCase() === string.toLowerCase()})
+ drivers.filter(function (d) {return d.toLowerCase() === string.toLowerCase()})
   
   
 }
+
+[1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
