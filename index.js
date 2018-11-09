84fac9be6b733685b502a2f1751868f06a101ea4
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-09 00:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function findMatching(drivers, string){
 
 function fuzzyMatch(drivers, string){
   let n = string.length 
-  let b = drivers.filter(function (d) { return 
-    d.slice(0,n+1) == string
+  let b = drivers.filter(function (d) { return d.slice(0,n+1) == string
   })
   return b 
 }
