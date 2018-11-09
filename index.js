52648408fa2db2aa5095a57f972b8396532e9f61
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-09 00:09:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file
 
 function findMatching(drivers, string){
- drivers.filter(function (d) {return d.toLowerCase() === string.toLowerCase()})
+ let b = drivers.filter(function (d) { d.toLowerCase() === string.toLowerCase()})
   
-  
+  return b 
 }
 
